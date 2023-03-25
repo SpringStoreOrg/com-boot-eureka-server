3adee6d05c984bd1f71a312a77c91ff4e32aa941
florinv23 <100446582+florinv23@users.noreply.github.com>
2023-03-25 23:14:32 +0200

    axon saga cloud

--- src/main/java/com/eureka/server/SpringEurekaServerApplication.java
+++ src/main/java/com/eureka/server/SpringEurekaServerApplication.java
@@ -2,10 +2,12 @@ package com.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer		// Enable eureka server
+@EntityScan(basePackages = {"org.axonframework.eventsourcing.eventstore.jpa"})
 public class SpringEurekaServerApplication {
 
 	public static void main(String[] args) {
